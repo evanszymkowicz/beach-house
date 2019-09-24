5c46c2712f74c651179b37efab3f7f4e7d5bc60e
Evan Szymkowicz <evan.szymkowicz@evanwolf.co>
2019-09-24 15:16:45 -0400

    react-app

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # beach-house
+
+Available scripts: </br>
+`npm start` </br>
+`npm build` </br>
+`npm test` </br>
+`npm eject` </br>
+
+
+## contentful
+Prebuilt backend structure that's more secure than WordPress but pretty expensive.
+</br>
+Requires two access keys stored in a local dot_env file
---
+++ src/data.js
@@ -0,0 +1,760 @@
+import room1 from "./images/details-1.jpeg";
+import room2 from "./images/details-2.jpeg";
+import room3 from "./images/details-3.jpeg";
+import room4 from "./images/details-4.jpeg";
+import img1 from "./images/room-1.jpeg";
+import img2 from "./images/room-2.jpeg";
+import img3 from "./images/room-3.jpeg";
+import img4 from "./images/room-4.jpeg";
+import img5 from "./images/room-5.jpeg";
+import img6 from "./images/room-6.jpeg";
+import img7 from "./images/room-7.jpeg";
+import img8 from "./images/room-8.jpeg";
+import img9 from "./images/room-9.jpeg";
+import img10 from "./images/room-10.jpeg";
+import img11 from "./images/room-11.jpeg";
+import img12 from "./images/room-12.jpeg";
+
+export default [
+  {
+    sys: {
+      id: "1"
+    },
+    fields: {
+      name: "single economy",
+      slug: "single-economy",
+      type: "single",
+      price:250,
+      size: 290,
+      capacity: 1,
+      pets: false,
+      breakfast: false,
+      featured: false,
+      description:
+        "Our single rooms are ideal for one person. Our single rooms overlook the street, and all have double glazing for extra effect.",
+      extras: [
+        "Plush pillows and breathable bed linens",
+        "Soft, oversized bath towels",
+        "Full-sized, pH-balanced toiletries",
+        "Complimentary refreshments",
+        "Adequate safety/security",
+        "WiFi",
+        "Comfortable beds"
+      ],
+      images: [
+        {
+          fields: {
+            file: {
+              url: img1
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room2
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room3
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room4
+            }
+          }
+        }
+      ]
+    }
+  },
+  {
+    sys: {
+      id: "2"
+    },
+    fields: {
+      name: "single basic",
+      slug: "single-basic",
+      type: "single",
+      price: 150,
+      size: 250,
+      capacity: 1,
+      pets: false,
+      breakfast: false,
+      featured: false,
+      description:
+        "A single room is for one person and contains a single bed, and will usually be quite small (but that's the norm for European hotel rooms anyway).",
+      extras: [
+        "Plush pillows and breathable bed linens",
+        "Soft, oversized bath towels",
+        "Full-sized, pH-balanced toiletries",
+        "Complimentary refreshments",
+        "Adequate safety/security",
+        "WiFi",
+        "Comfortable beds"
+      ],
+      images: [
+        {
+          fields: {
+            file: {
+              url: img2
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room2
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room3
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room4
+            }
+          }
+        }
+      ]
+    }
+  },
+  {
+    sys: {
+      id: "3"
+    },
+    fields: {
+      name: "single standard",
+      slug: "single-standard",
+      type: "single",
+      price: 250,
+      size: 300,
+      capacity: 1,
+      pets: true,
+      breakfast: false,
+      featured: false,
+      description:
+        "You can stretch out in a comfortable suite with a full kitchen and all the space you need to relax, be productive and stay connected",
+      extras: [
+        "Plush pillows and breathable bed linens",
+        "Soft, oversized bath towels",
+        "Full-sized, pH-balanced toiletries",
+        "Complimentary refreshments",
+        "Adequate safety/security",
+        "WiFi",
+        "Comfortable beds"
+      ],
+      images: [
+        {
+          fields: {
+            file: {
+              url: img3
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room2
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room3
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room4
+            }
+          }
+        }
+      ]
+    }
+  },
+  {
+    sys: {
+      id: "4"
+    },
+    fields: {
+      name: "single deluxe",
+      slug: "single-deluxe",
+      type: "single",
+      price: 300,
+      size: 400,
+      capacity: 1,
+      pets: true,
+      breakfast: true,
+      featured: false,
+      description:
+        "Bedrooms and bathrooms with amenities. Kitchen with full size refrigerator and a flexible workspace and three TVs.",
+      extras: [
+        "Plush pillows and breathable bed linens",
+        "Soft, oversized bath towels",
+        "Full-sized, pH-balanced toiletries",
+        "Complimentary refreshments",
+        "Adequate safety/security",
+        "WiFi",
+        "Comfortable beds"
+      ],
+      images: [
+        {
+          fields: {
+            file: {
+              url: img4
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room2
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room3
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room4
+            }
+          }
+        }
+      ]
+    }
+  },
+  {
+    sys: {
+      id: "5"
+    },
+    fields: {
+      name: "double economy",
+      slug: "double-economy",
+      type: "double",
+      price: 200,
+      size: 300,
+      capacity: 2,
+      pets: false,
+      breakfast: false,
+      featured: false,
+      description:
+        "Ideal for two people. Our single rooms overlook the street, and all have double glazing for extra effect.",
+      extras: [
+        "Plush pillows and breathable bed linens",
+        "Soft, oversized bath towels",
+        "Full-sized, pH-balanced toiletries",
+        "Complimentary refreshments",
+        "Adequate safety/security",
+        "WiFi",
+        "Comfortable beds"
+      ],
+      images: [
+        {
+          fields: {
+            file: {
+              url: img5
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room2
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room3
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room4
+            }
+          }
+        }
+      ]
+    }
+  },
+  {
+    sys: {
+      id: "6"
+    },
+    fields: {
+      name: "double basic",
+      slug: "double-basic",
+      type: "double",
+      price: 250,
+      size: 350,
+      capacity: 2,
+      pets: false,
+      breakfast: false,
+      featured: false,
+      description:
+        "",
+      extras: [
+        "Plush pillows and breathable bed linens",
+        "Soft, oversized bath towels",
+        "Full-sized, pH-balanced toiletries",
+        "Complimentary refreshments",
+        "Adequate safety/security",
+        "WiFi",
+        "Comfortable beds"
+      ],
+      images: [
+        {
+          fields: {
+            file: {
+              url: img6
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room2
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room3
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room4
+            }
+          }
+        }
+      ]
+    }
+  },
+  {
+    sys: {
+      id: "7"
+    },
+    fields: {
+      name: "double standard",
+      slug: "double-standard",
+      type: "double",
+      price: 300,
+      size: 400,
+      capacity: 2,
+      pets: true,
+      breakfast: false,
+      featured: false,
+      description:
+        "Some standard double rooms are meant for a couple; these have just one queen- or king-sized bed. Other standard double rooms have two double or queen-sized beds placed side by side a few feet apart.",
+      extras: [
+        "Plush pillows and breathable bed linens",
+        "Soft, oversized bath towels",
+        "Full-sized, pH-balanced toiletries",
+        "Complimentary refreshments",
+        "Adequate safety/security",
+        "WiFi",
+        "Comfortable beds"
+      ],
+      images: [
+        {
+          fields: {
+            file: {
+              url: img7
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room2
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room3
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room4
+            }
+          }
+        }
+      ]
+    }
+  },
+  {
+    sys: {
+      id: "8"
+    },
+    fields: {
+      name: "double deluxe",
+      slug: "double-deluxe",
+      type: "double",
+      price: 400,
+      size: 500,
+      capacity: 2,
+      pets: true,
+      breakfast: true,
+      featured: true,
+      description:
+        "Two comfortable double beds and a kitchenette with microwave and refrigerator among other convenient amenities.",
+      extras: [
+        "Plush pillows and breathable bed linens",
+        "Soft, oversized bath towels",
+        "Full-sized, pH-balanced toiletries",
+        "Complimentary refreshments",
+        "Adequate safety/security",
+        "WiFi",
+        "Comfortable beds"
+      ],
+      images: [
+        {
+          fields: {
+            file: {
+              url: img8
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room2
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room3
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room4
+            }
+          }
+        }
+      ]
+    }
+  },
+  {
+    sys: {
+      id: "9"
+    },
+    fields: {
+      name: "family economy",
+      slug: "family-economy",
+      type: "family",
+      price: 300,
+      size: 650,
+      capacity: 5,
+      pets: false,
+      breakfast: false,
+      featured: false,
+      description:
+        "Can accommodate up to 5 people. Room has 3 single beds and a sofa that turns into a bed. Room can also fit 1 extra bed or 1 baby cot.",
+      extras: [
+        "Plush pillows and breathable bed linens",
+        "Soft, oversized bath towels",
+        "Full-sized, pH-balanced toiletries",
+        "Complimentary refreshments",
+        "Adequate safety/security",
+        "WiFi",
+        "Comfortable beds"
+      ],
+      images: [
+        {
+          fields: {
+            file: {
+              url: img9
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room2
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room3
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room4
+            }
+          }
+        }
+      ]
+    }
+  },
+  {
+    sys: {
+      id: "10"
+    },
+    fields: {
+      name: "family basic",
+      slug: "family-basic",
+      type: "family",
+      price: 350,
+      size: 550,
+      capacity: 5,
+      pets: false,
+      breakfast: false,
+      featured: false,
+      description:
+        "Can accommodate up to 5 people. Room has 3 single beds and a sofa that turns into a bed. Room can also fit 1 extra bed or 1 baby cot.",
+      extras: [
+        "Plush pillows and breathable bed linens",
+        "Soft, oversized bath towels",
+        "Full-sized, pH-balanced toiletries",
+        "Complimentary refreshments",
+        "Adequate safety/security",
+        "WiFi",
+        "Comfortable beds"
+      ],
+      images: [
+        {
+          fields: {
+            file: {
+              url: img10
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room2
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room3
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room4
+            }
+          }
+        }
+      ]
+    }
+  },
+  {
+    sys: {
+      id: "11"
+    },
+    fields: {
+      name: "family standard",
+      slug: "family-standard",
+      type: "family",
+      price: 400,
+      size: 600,
+      capacity: 5,
+      pets: true,
+      breakfast: false,
+      featured: false,
+      description:
+        "Also called the Family Room. The softa extends out to a full bed.",
+      extras: [
+        "Plush pillows and breathable bed linens",
+        "Soft, oversized bath towels",
+        "Full-sized, pH-balanced toiletries",
+        "Complimentary refreshments",
+        "Adequate safety/security",
+        "WiFi",
+        "Comfortable beds"
+      ],
+      images: [
+        {
+          fields: {
+            file: {
+              url: img11
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room2
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room3
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room4
+            }
+          }
+        }
+      ]
+    }
+  },
+  {
+    sys: {
+      id: "12"
+    },
+    fields: {
+      name: "family deluxe",
+      slug: "family-deluxe",
+      type: "family",
+      price: 500,
+      size: 1000,
+      capacity: 6,
+      pets: true,
+      breakfast: true,
+      featured: true,
+      description:
+        "Similar to the family standard with same ammenteties but with enhanced features.",
+      extras: [
+        "Plush pillows and breathable bed linens",
+        "Soft, oversized bath towels",
+        "Full-sized, pH-balanced toiletries",
+        "Complimentary refreshments",
+        "Adequate safety/security",
+        "WiFi",
+        "Comfortable beds"
+      ],
+      images: [
+        {
+          fields: {
+            file: {
+              url: img12
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room2
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room3
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room4
+            }
+          }
+        }
+      ]
+    }
+  },
+  {
+    sys: {
+      id: "13"
+    },
+    fields: {
+      name: "presidential",
+      slug: "presidential-room",
+      type: "presidential",
+      price: 600,
+      size: 1200,
+      capacity: 10,
+      pets: true,
+      breakfast: true,
+      featured: true,
+      description:
+        "Fine Egyptian cotton linens, and plush down duvet for supreme pampering.",
+      extras: [
+        "Plush pillows and breathable bed linens",
+        "Soft, oversized bath towels",
+        "Full-sized, pH-balanced toiletries",
+        "Complimentary refreshments",
+        "Adequate safety/security",
+        "WiFi",
+        "Comfortable beds"
+      ],
+      images: [
+        {
+          fields: {
+            file: {
+              url: room1
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room2
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room3
+            }
+          }
+        },
+        {
+          fields: {
+            file: {
+              url: room4
+            }
+          }
+        }
+      ]
+    }
+  }
+];
